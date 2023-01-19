5343d0a74a53b9198ecafb2332377b92676c329a
Lucas Minter <lsminteris@gmail.com>
2023-01-19 12:26:42 -0800

    converted from css to tailwind

--- components/BlogList.js
+++ components/BlogList.js
@@ -10,17 +10,17 @@ const BlogList = ({ allBlogs }) => {
 
   return (
     <>
-      <div className={styles.grid}>
+      <div className="flex items-center justify-center flex-wrap">
         {allBlogs.data.map(post => (
           <div key={post.attributes.slug}>
             <Link href={{ pathname: `/posts/${post.attributes.slug}` }} passHref>
-              <h2 className={styles.card}>
+              <h2 className="border-solid border m-4 p-6 text-left no-underline hover:border-cyan-800 hover:text-cyan-800 rounded-xl">
                   {post.attributes.title}
                 <p>
                   by {post.attributes.author} on {reformatDate(post.attributes.date)}
                 </p>
               </h2>
-            </Link> 
+            </Link>
           </div>
         ))}
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ export default function Home() {
     <div className="h-screen">
     
       <div className="grid place-content-center">
-        <text className="text-4xl">My Blog</text>
-        <a className="hover:bg-green-200" href="/posts">Checkout my posts</a>
+        <text className="m-4 p-6 no-underline text-2xl">My Blog</text>
+        <a className="border-solid border m-4 p-6 text-left no-underline hover:border-cyan-800 hover:text-cyan-800 rounded-xl" href="/posts">Checkout my posts</a>
       </div>
     </div>
   );
