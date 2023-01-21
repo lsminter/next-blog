27a658e1ff106d6a254ef98132c14feec4c51895
Lucas Minter <lsminteris@gmail.com>
2023-01-20 20:41:40 -0800

    updated export syntax

--- client.js
+++ client.js
@@ -1,9 +1,8 @@
 const sanityClient = require('@sanity/client')
 
-export default sanityClient({
+module.exports = sanityClient({
   projectId: '0zxiq0ro', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
   ignoreBrowserTokenWarning: true
-
 })
