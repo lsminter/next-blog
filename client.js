9e744bbe01fe62b24b581686489aa672d579a3f5
Lucas Minter <lsminteris@gmail.com>
2023-01-21 12:37:43 -0800

    configured netlify, fixed sanity client

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const sanityClient = require('@sanity/client')
 
-module.exports = sanityClient({
+export const sanityClient = sanityClient({
   projectId: '0zxiq0ro', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
