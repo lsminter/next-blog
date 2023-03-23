90a2f3b4ee0446f99d3cbb074480adfbc58380be
Lucas Minter <lsminteris@gmail.com>
2023-03-23 13:33:02 -0700

    edited and uploaded new resume

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function Home() {
         <a 
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           target="_blank"
-          href='/files/LinkedIn-Resume-Lucas-Minter.pdf' 
+          href='/files/Lucas-Minter-Resume.pdf' 
           rel="noopener noreferrer"
         >
         Download Resume
