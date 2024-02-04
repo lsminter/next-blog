1298f1efb8dce24e61b77312f403ec2f8aceb238
lsminter <lsminteris@gmail.com>
2024-02-04 13:11:21 -0800

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,13 @@ import '../styles/globals.css'
 import Footer from '../components/footer'
 import Header from '../components/header'
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className='h-full px-4 sm:px-12 bg-background'>
       <Header />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZSV8NJ1KXL" />
       <Component {...pageProps} />
       <Footer />
       <Analytics />
